14f4bcf92fb8b39a50c284a0ed7cc26ddbd3a13a
Rafael Perez <rafaelperez@MacBook-Pro-2.local>
2025-09-30 19:41:57 -0400

    Versión5

--- components/icons/CoffeeIcon.tsx
+++ components/icons/CoffeeIcon.tsx
@@ -1,8 +1,7 @@
-
 import React from 'react';
 
-export const CoffeeIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
+export const CheckIcon: React.FC<React.SVGProps<SVGSVGElement>> = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" {...props}>
-    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5h3m-6.75-6.75h9.75m-9.75 0a2.25 2.25 0 012.25-2.25h5.25a2.25 2.25 0 012.25 2.25m-9.75 0a2.25 2.25 0 002.25 2.25h5.25a2.25 2.25 0 002.25-2.25m-15-3.375a2.25 2.25 0 012.25-2.25h15a2.25 2.25 0 012.25 2.25v6a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25v-6z" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
   </svg>
 );
